6d050a695b3e4db3eaa7e867babf45e20c471399
szymlan <31695143+szymlan@users.noreply.github.com>
2017-12-02 11:03:01 +0100

    Update cloud.php
    Dodano informację o ostatnim nieudanym logowaniu.

--- cloud.php
+++ cloud.php
@@ -17,7 +17,16 @@
     <body>
     <?php
         echo "<p>Witaj ".$_SESSION['Imie'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
-    
+    	$dbhost="serwer1783357.home.pl"; $dbuser="25516664_0000007"; $dbpassword="hosting07"; $dbname="25516664_0000007";
+        $polaczenie = mysqli_connect ($dbhost, $dbuser, $dbpassword, $dbname);
+        mysqli_select_db ($polaczenie, $dbname);
+        $log = $_SESSION['Login'];
+        $rezultat = mysqli_query ($polaczenie, "SELECT * FROM logi WHERE Login='$log' order by id DESC LIMIT 1");
+        while ($wiersz = mysqli_fetch_array ($rezultat))    {
+        $datalog = $wiersz[1];
+        $ip = $wiersz[2];}
+        echo "Ostatnia nieudana próba logowania miała miejsce $datalog z adresu $ip!";
+       
         ?>
         <br><br><a href="/"> Powrót do strony głównej </a></center>
 
