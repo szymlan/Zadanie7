6fbd2b44a00b0ee53f5d9269bafb02327f914cd8
szymlan <31695143+szymlan@users.noreply.github.com>
2017-12-08 11:07:59 +0100

    Update folder.php
    Doprowadzono plik do wersji finalnej, umożliwiono dodawanie i pobieranie plików, przyporządkowano do poszczególnych typów pliku odpowiednie ikony.

--- uzytkownicy/folder.php
+++ uzytkownicy/folder.php
@@ -18,6 +18,11 @@
     .w3-allerta {
       font-family: "Allerta Stencil", Sans-serif;
     }
+    img{
+    margin-left:30px;
+    position: relaive;
+    width: 80%;
+    }
     </style>
 
     </head>
@@ -26,13 +31,49 @@
         <p class="w3-xlarge">
     <?php
         $log = $_SESSION['Login'];
-        echo "<p><div style='text-align: left;'>/".$log.'</div>[ <a href="logout.php">Wyloguj się!</a> ]</p>';?></p></div>
-        <form method="post" action="dodaj.php">
-        Utwórz folder:
-        <br><input type="text" name="folder" maxlength="40" required><br>
-        <input type="submit" value="Utwórz"/>
-        </form>
-        <br><br><a href="/"> Powrót do strony głównej </a></center>
+        echo "<p><div style='text-align: left;'>/".$log."</div>[ <a href='logout.php'>Wyloguj się!</a> ]</p>";?></p></div><br>
+        <div style="text-align: left; margin-left: 10px;">Wgraj plik:<br><br>
+        <form action="odbierz.php" method="POST" ENCTYPE="multipart/form-data"><div style="border-style: solid; border-color: grey; width:328px;">
+        <input type="file" name="plik"/></div><br><input type="submit" value="Wyślij plik"/></form>
+        <form method="post" action="dodaj.php"> <?php
+        if (isset ($_SESSION['upload'])) echo $_SESSION['upload'];
+        if (isset ($_SESSION['failedupload'])) echo $_SESSION['failedupload'];
+        ?><br>
+        Utwórz nowy folder:<br>
+        <br><input type="text" name="folder" maxlength="40" required><br><br>
+		<input type="submit" value="Utwórz"/></div>
+        </form><br>
+       <div class="w3-container w3-black w3-center w3-allerta">
+        <p class="w3-xsmall">Twoje pliki:</p></div><br>
+        <? 
+        
+         $files1 = array_diff(scandir($log), array('.', '..'));
+        foreach($files1 as $file)
+        {
+            if(is_dir($log . DIRECTORY_SEPARATOR . $file)){
+                echo "<div class='item'><a href='podfolder.php?podkatalog=$file'><img src='folder.png'><div class='tekst'>$file</div></a></div>";
+            }
+            else{
+                echo "<div class='item'><a href='$log/$file' download='$file'><img src='plik.jpg'><div class='tekst'>$file</div></a></div>";
+            }
+            } 
+        ?>
+        <style>
+            .item {
+            vertical-align: top;
+            display: inline-block;
+            text-align: center;
+            width: 100px;
+        }
+          .tekst{
+            margin-left:38px;
+            width: 65px;
+            position: absolute;
+             word-break: break-all;
+        }
+        </style>
+        <br><br>
+        <br><br><span style="color:blue"><a href="/"> Powrót do strony głównej</a> </span></center>
 
     </body>
 </html>
